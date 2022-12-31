59f2c637259d94b0db0546e9d45e71fbcca7edf9
E.Christo Raymonde <101086026+ec7719@users.noreply.github.com>
2022-12-31 18:53:18 +0530

    Add files via upload

---
+++ Addition of strings.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main (){
+
+	int a[100];
+	int n;
+	int i;
+	int add;
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+scanf("%d",a[i]);
+
+add=a[1]+a[2];}
+
+for(i=0;i<n;i++){
+	printf("%d",add);
+}
+	return 0;
+}
